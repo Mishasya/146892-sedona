b2645ff5d57c2c17f33b59c184c34a2ed5ebed80
Mishasya <mishasya@bk.ru>
2018-09-13 10:16:11 +0300

    исправления

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 var button = document.querySelector(".search-btn");
 var popup = document.querySelector(".modal");
+var form = popup.querySelector(".form-search");
+var dateIn = popup.querySelector("[name=date-in]");
+var dateOut = popup.querySelector("[name=date-out]");
+var adult = popup.querySelector("[name=adult-count]");
+var child = popup.querySelector("[name=child-count]");
 
 button.addEventListener("click", function(evt) {
-	evt.preventDefault();
-	popup.classList.toggle("modal-show");
+  evt.preventDefault();
+  popup.classList.toggle("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!dateIn.value || !dateOut.value || !adult.value || !child.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+  }
 });
